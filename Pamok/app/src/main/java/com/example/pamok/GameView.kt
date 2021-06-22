8027215f215cbaf0253df034c537a5f965c6a676
s16663@pjwstk.edu.pl <s16663@pjwstk.edu.pl>
2021-06-23 00:02:57 +0200

    Finished input detection, targets creation and collision system, plus points summing

--- Pamok/app/src/main/java/com/example/pamok/GameView.kt
+++ Pamok/app/src/main/java/com/example/pamok/GameView.kt
@@ -3,36 +3,67 @@ package com.example.pamok
 import android.content.Context
 import android.content.res.Resources
 import android.graphics.*
+import android.util.Log
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.SurfaceView
+import androidx.core.view.GestureDetectorCompat
 
-class GameView : SurfaceView, Runnable {
-
+class GameView(context: Context) : SurfaceView(context), Runnable, GestureDetector.OnGestureListener {
     private val maxFPS: Int = 60
-    private val framePeriod: Int = 1000 / maxFPS
-    private val targetsList: MutableList<TargetObject> = mutableListOf()
-    private var touchPoint: Point? = null
-
     private var currentSpawnTime: Int = 0
+    private val framePeriod: Int = 1000 / maxFPS
+    private var flingLine: Pair<Point?, Point?> = Pair(null, null)
+    private val maxTargets: Int = 1
+    private var mDetector: GestureDetectorCompat = GestureDetectorCompat(context, this)
+    private var points: Int = 0
     private var running: Boolean = false
     private var spawnTime: Int = 1 * maxFPS
+    private var targetsList: MutableList<TargetObject> = mutableListOf()
     private var thread: Thread? = null
 
-    constructor(context: Context) : super(context)
+    override fun onDown(e: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onFling(event1: MotionEvent, event2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        flingLine = Pair(Point(event1.x.toInt(), event1.y.toInt()), Point(event2.x.toInt(), event2.y.toInt()))
+        return true
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+    }
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+        return true
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        return true
+    }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
-        if (touchPoint == null) {
-            touchPoint = Point(event.x.toInt(), event.y.toInt())
-
-            return true
+        return if (mDetector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
         }
-
-        return false
     }
 
     fun pause() {
         running = false
         thread!!.join()
+    }
+
+
+    private fun render(canvas: Canvas) {
+        canvas.drawRGB(60, 60, 60)
+        targetsList.forEach {
+            it.draw(canvas)
+        }
     }
 
     fun resume() {
@@ -46,67 +77,51 @@ class GameView : SurfaceView, Runnable {
             val canvas = holder.lockCanvas()
             if (canvas != null) {
                 synchronized(holder) {
-                    val startTime = System.currentTimeMillis()
-
+                    val startTime: Long = System.currentTimeMillis()
                     update()
                     render(canvas)
-
-                    val timeDiff = System.currentTimeMillis() - startTime
-                    val sleepTime = framePeriod - timeDiff
-
+                    val timeDiff: Long = System.currentTimeMillis() - startTime
+                    val sleepTime: Long = framePeriod - timeDiff
                     if (sleepTime > 0) {
                         Thread.sleep(sleepTime)
                     }
-
                     holder.unlockCanvasAndPost(canvas)
                 }
             }
         }
+        //TODO: "you lost"
     }
 
     private fun update() {
-        // Targets killing
-        if (touchPoint != null) {
+        if (flingLine.first != null) {
             targetsList.forEach {
-                val r: Rect = it.getBounds()
-
-                if (r.contains(touchPoint!!.x, touchPoint!!.y) && it.getPixel(touchPoint!!.x, touchPoint!!.y) != Color.TRANSPARENT) {
-                    it.kill()
+                if (it.intersectsWith(flingLine)) {
+                    it.cut()
                 }
             }
-
-            targetsList.removeIf {
-                !it.isAlive()
+            points += targetsList.count{
+                it.isCut()
             }
-
-            touchPoint = null
+            targetsList.removeIf {
+                it.isCut()
+            }
+            flingLine = Pair(null, null)
         }
-
-        // Targets spawning
         if (currentSpawnTime-- == 0) {
-            targetsList.add(TargetObject(
-                Resources.getSystem().displayMetrics.widthPixels / 2,
-                Resources.getSystem().displayMetrics.heightPixels / 2,
-                context.resources
-            ))
-
-            currentSpawnTime = spawnTime
+            if (targetsList.count() < maxTargets)
+            {
+                targetsList.add(TargetObject(
+                    Resources.getSystem().displayMetrics.widthPixels / 2,
+                    Resources.getSystem().displayMetrics.heightPixels,
+                    context.resources
+                ))
+            }
+            currentSpawnTime = spawnTime--
         }
-
-        // Targets movement
         targetsList.forEach {
-            it.move()
-        }
-    }
-
-    private fun render(canvas: Canvas) {
-        // Background
-        canvas.drawRGB(60, 60, 60)
-
-        // Targets
-        targetsList.forEach {
-            it.image?.bounds = it.getBounds()
-            it.image?.draw(canvas)
+            if (!it.move()) {
+                running = false
+            }
         }
     }
 }
--- Pamok/app/src/main/java/com/example/pamok/TargetObject.kt
+++ Pamok/app/src/main/java/com/example/pamok/TargetObject.kt
@@ -1,57 +1,92 @@
 package com.example.pamok
 
 import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Point
 import android.graphics.Rect
 import android.graphics.drawable.Drawable
+import android.util.Log
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toBitmap
+import kotlin.math.abs
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.math.sqrt
 
 class TargetObject(startX: Int, startY: Int, resources: Resources) {
-
-    private val width: Int = 250
+    private var baseY: Int = startY
+    private val createTime: Long = System.currentTimeMillis()
+    private val g: Double = 9.80665
     private val height: Int = 250
-    val image: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.target_24, null)
+    private val image: Drawable? = ResourcesCompat.getDrawable(resources, R.drawable.target_24, null)
+    private var isCut: Boolean = false
+    private val v: Double = sqrt(4 / 3 * startY * g)
+    private val width: Int = height
+    private var x: Int = startX
+    private var y: Int = baseY
 
-    private var x: Int = 0
-    private var y: Int = 0
-    private var speed: Pair<Double, Double> = Pair(10.0, 10.0)
-    private var alive: Boolean = true
-
-    init {
-        x = startX
-        y = startY
+    fun cut() {
+        isCut = true
     }
 
-    fun move() {
-        if ((x - height / 2 <= 0) || (x + height / 2 >= Resources.getSystem().displayMetrics.widthPixels)) {
-            speed = Pair(-speed.first, speed.second)
+    fun draw(canvas: Canvas) {
+        if (!isCut) {
+            image?.bounds = getBounds()
+            image?.draw(canvas)
         }
-
-        x += speed.first.toInt()
-
-        if ((y - width / 2 <= 0) || (y + width / 2 >= Resources.getSystem().displayMetrics.heightPixels)) {
-            speed = Pair(speed.first, -speed.second)
-        }
-
-        y += speed.second.toInt()
     }
 
-    fun kill() {
-        alive = false
-    }
-
-    fun getBounds() : Rect {
+    private fun getBounds(): Rect {
         return Rect(x - width / 2, y - height / 2, x + width / 2, y + height / 2)
     }
 
-    fun getPixel(coordX: Int, coordY: Int): Int?
-    {
-        val r = getBounds()
-
+    private fun getPixelColor(coordX: Int, coordY: Int): Int? {
+        val r: Rect = getBounds()
         return image?.toBitmap(width, height)?.getPixel(coordX - r.left, coordY - r.top)
     }
 
-    fun isAlive(): Boolean {
-        return alive
+    fun intersectsWith(line: Pair<Point?, Point?>): Boolean {
+        val r: Rect = getBounds()
+        val lineX: Int = abs(line.second!!.x - line.first!!.x)
+        val lineY: Int = line.second!!.y - line.first!!.y
+        val ratio: Double = lineY / lineX.toDouble()
+        val points: MutableList<Point> = mutableListOf()
+        for (i: Int in 1..lineX) {
+            val point = Point(line.first!!.x + if (line.first!!.x > line.second!!.x) -i else i, line.first!!.y + (i * ratio).roundToInt())
+            if (r.contains(point.x, point.y) && isEdgePixel(point.x, point.y)) {
+                points.add(point)
+            }
+        }
+        return points.count() > 1
+    }
+
+    fun isCut(): Boolean {
+        return isCut
+    }
+
+    private fun isEdgePixel(coordX: Int, coordY: Int): Boolean {
+        if (getPixelColor(coordX, coordY) != Color.TRANSPARENT) {
+            val surrounding: MutableList<Int?> = mutableListOf()
+            surrounding.add(getPixelColor(coordX - 1, coordY - 1))
+            surrounding.add(getPixelColor(coordX, coordY - 1))
+            surrounding.add(getPixelColor(coordX + 1, coordY - 1))
+            surrounding.add(getPixelColor(coordX - 1, coordY))
+            surrounding.add(getPixelColor(coordX + 1, coordY))
+            surrounding.add(getPixelColor(coordX - 1, coordY + 1))
+            surrounding.add(getPixelColor(coordX, coordY + 1))
+            surrounding.add(getPixelColor(coordX + 1, coordY + 1))
+            return surrounding.any {
+                it == Color.TRANSPARENT || it == null
+            }
+        } else {
+            return false
+        }
+    }
+
+    fun move(): Boolean {
+        val t: Double = (System.currentTimeMillis() - createTime) / 1000.0
+        y = baseY - (v * t - (g * t.pow(2.0) / 2)).roundToInt()
+        return y < baseY || t < 1.0
     }
 }
